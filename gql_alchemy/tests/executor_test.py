65068a1b85220192e549d559aaf7535b068b86aa
Dmitry Maslennikov <maslennikovdm@gmail.com>
2018-02-01 10:06:17 +0700

    Some executor tests

---
+++ gql_alchemy/tests/executor_test.py
@@ -0,0 +1,61 @@
+import json
+import typing as t
+import unittest
+
+import gql_alchemy.schema as s
+from ..executor import Executor
+from ..utils import PrimitiveType
+
+
+class ExecutorTest(unittest.TestCase):
+    def assertQueryResult(self, expected: str, schema: s.Schema, resolver: t.Any,
+                          query: str, variables: t.Optional[t.Mapping[str, PrimitiveType]] = None,
+                          op_name: t.Optional[str] = None) -> None:
+        e = Executor(schema, resolver)
+        result = e.query(query, variables if variables is not None else {}, op_name)
+        self.assertEqual(expected, json.dumps(result, sort_keys=True))
+
+    def test_select_sclar(self):
+        class Resolver:
+            def foo(self, args):
+                return 3
+
+        self.assertQueryResult(
+            '{"foo": 3}',
+            s.Schema(
+                [],
+                s.Object("__Query", {
+                    "foo": s.Int
+                })
+            ),
+            Resolver(),
+            "{foo}"
+        )
+
+    def test_select_with_rguments(self):
+        class FooResolver:
+            type = "Foo"
+
+            def __init__(self, foo: int):
+                self.__foo = foo
+
+            def bar(self, args):
+                return args["abc"] + self.__foo
+
+        class Resolver:
+            def foo(self, args):
+                return FooResolver(args["foo"])
+
+        self.assertQueryResult(
+            '{"foo": {"bar": 7}}',
+            s.Schema(
+                [
+                    s.Object("Foo", {"bar": s.Field(s.Int, {"abc": s.Int})})
+                ],
+                s.Object("__Query", {
+                    "foo": s.Field("Foo", {"foo": s.Int})
+                })
+            ),
+            Resolver(),
+            "{foo(foo: 3){bar(abc: 4)}}"
+        )
